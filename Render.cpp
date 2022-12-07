fd6672102ee9cf04ab51250ae2ad60d428589b51
atrox31 <atrox31@gmail.com>
2022-12-07 16:04:47 +0100

    add draw rect round

--- Render.cpp
+++ Render.cpp
@@ -1,14 +1,15 @@
 #include "Render.h"
+
+#include <utility>
 #include "Func.h"
 #include "ColorDefinitions.h"
 #include "Convert.h"
 Render* Render::_instance = nullptr;
 
-void Render::CreateRender(int width, int height)
+void Render::CreateRender(const int width, const int height)
 {
-	if (_instance != nullptr) {
-		delete _instance;
-	}
+	// if instance exists create new
+	delete _instance;
 	_instance = new Render();
 
 	_instance->width = width;
@@ -85,9 +86,9 @@ Render::~Render()
 	DestroyRender();
 }
 
-void Render::DrawTexture(GPU_Image* texture, vec2f postion, vec2f scale, float angle, float alpha)
+void Render::DrawTexture(GPU_Image* texture, const vec2f& postion, const vec2f& scale, float angle, float alpha)
 {
-	bool alpha_flag = alpha < 1.0f;
+	const bool alpha_flag = alpha < 1.0f;
 	if (alpha_flag) {
 		if (alpha < 0.0f) alpha = 0.0f;
 		if (alpha > 1.0f) alpha = 1.0f;
@@ -95,18 +96,18 @@ void Render::DrawTexture(GPU_Image* texture, vec2f postion, vec2f scale, float a
 		//GPU_GetBlendModeFromPreset(GPU_BlendPresetEnum::GPU_BLEND_MOD_ALPHA);
 		GPU_SetColor(texture, { (Uint8)255,(Uint8)255,(Uint8)255,(Uint8)(alpha * 255.f) });
 	}
-	GPU_BlitTransform(texture, NULL, _instance->_screenTexture_target, postion.x, postion.y, Convert::RadiansToDegree(angle), scale.x, scale.y);
+	GPU_BlitTransform(texture, nullptr, _instance->_screenTexture_target, postion.x, postion.y, Convert::RadiansToDegree(angle), scale.x, scale.y);
 	if (alpha_flag) {
 		GPU_SetBlending(texture, false);
 
 		GPU_SetColor(texture, C_FULL);
 	}
 }
-void Render::DrawSprite(Sprite* sprite, vec2f postion, int frame) {
+void Render::DrawSprite(Sprite* sprite, const vec2f& postion, int frame) {
 	DrawSprite_ex(sprite, postion.x, postion.y, frame, 1.0f, 1.0f, (float)sprite->GetCenterX(), (float)sprite->GetCenterY(), 0.0f, 1.0f);
 }
 
-void Render::DrawSprite_ex(Sprite* sprite, float posX, float posY, int frame, float scaleX, float scaleY, float centerX, float centerY, float angle, float alpha) {
+void Render::DrawSprite_ex(Sprite* sprite, float pos_x, float pos_y, int frame, float scale_x, float scale_y, float center_x, float center_y, float angle, float alpha) {
 	if (sprite == nullptr) return;
 
 	const int spr_max_frames = sprite->GetMaxFrame();
@@ -119,108 +120,157 @@ void Render::DrawSprite_ex(Sprite* sprite, float posX, float posY, int frame, fl
 		}
 	}
 
-	GPU_Image* target = sprite->GetFrame(frame);
+	GPU_Image* source = sprite->GetFrame(frame);
 
 	bool alpha_flag = alpha < 1.0f;
 	if (alpha_flag) {
 		if (alpha < 0.0f) alpha = 0.0f;
 		if (alpha > 1.0f) alpha = 1.0f;
-		GPU_SetBlending(target, true);
+		GPU_SetBlending(source, true);
 		//GPU_GetBlendModeFromPreset(GPU_BlendPresetEnum::GPU_BLEND_MOD_ALPHA);
-		GPU_SetColor(target, { (Uint8)255,(Uint8)255,(Uint8)255,(Uint8)(alpha * 255) });
+		GPU_SetColor(source, { (Uint8)255,(Uint8)255,(Uint8)255,(Uint8)(alpha * 255) });
 	}
 
 	GPU_BlitTransformX(
-		target,
-		NULL,
+		source,
+		nullptr,
 		_instance->_screenTexture_target,
-		posX, posY,
-		centerX, centerY,
+		pos_x, pos_y,
+		center_x, center_y,
 		Convert::RadiansToDegree(angle),
-		scaleX, scaleY
+		scale_x, scale_y
 	);
 
 	if (alpha_flag) {
-		GPU_SetBlending(target, false);
-		GPU_SetColor(target, C_FULL);
+		GPU_SetBlending(source, false);
+		GPU_SetColor(source, C_FULL);
+	}
+}
+void Render::DrawSpriteBox(const Sprite* sprite, GPU_Rect& box, int frame, const float& angle, float alpha) {
+	if (sprite == nullptr) return;
+
+	const int spr_max_frames = sprite->GetMaxFrame();
+	if (spr_max_frames == 1) {
+		frame = 0;
+	}
+	else {
+		if (frame > spr_max_frames) {
+			frame = frame % spr_max_frames;
+		}
+	}
+
+	GPU_Image* source = sprite->GetFrame(frame);
+
+	const bool alpha_flag = alpha < 1.0f;
+	if (alpha_flag) {
+		if (alpha < 0.0f) alpha = 0.0f;
+		if (alpha > 1.0f) alpha = 1.0f;
+		GPU_SetBlending(source, true);
+		//GPU_GetBlendModeFromPreset(GPU_BlendPresetEnum::GPU_BLEND_MOD_ALPHA);
+		GPU_SetColor(source, { (Uint8)255,(Uint8)255,(Uint8)255,(Uint8)(alpha * 255) });
+	}
+
+	GPU_BlitRect(source, nullptr, _instance->_screenTexture_target, &box);
+	
+	if (alpha_flag) {
+		GPU_SetBlending(source, false);
+		GPU_SetColor(source, C_FULL);
 	}
 }
 
-void Render::DrawRect(Rect rect, SDL_Color color)
+void Render::DrawRect(const GPU_Rect rect, const SDL_Color color)
 {
-	GPU_Rectangle(_instance->_screenTexture_target, rect.x, rect.y, rect.w, rect.h, color);
+	GPU_Rectangle2(_instance->_screenTexture_target, rect, color);
 }
 
-void Render::DrawRect_wh(Rect rect, SDL_Color color)
+void Render::DrawRect_wh(const GPU_Rect rect, const SDL_Color color)
 {
 	GPU_Rectangle(_instance->_screenTexture_target, rect.x, rect.y, rect.x + rect.w, rect.y + rect.h, color);
 }
 
-void Render::DrawRectFilled(Rect rect, SDL_Color color)
+void Render::DrawRectFilled(const GPU_Rect rect, const SDL_Color color)
 {
-	GPU_RectangleFilled(_instance->_screenTexture_target, rect.x, rect.y, rect.w, rect.h, color);
+	GPU_RectangleFilled2(_instance->_screenTexture_target, rect, color);
+}
+
+void Render::DrawRectRounded(const GPU_Rect rect, const float round_factor, const SDL_Color color)
+{
+	GPU_RectangleRound(_instance->_screenTexture_target, rect.x, rect.y, rect.w, rect.h, round_factor, color);
+}
+
+void Render::DrawRectRoundedFilled(const GPU_Rect rect, const float round_factor, const SDL_Color color)
+{
+	GPU_RectangleRoundFilled(_instance->_screenTexture_target, rect.x, rect.y, rect.w, rect.h, round_factor, color);
 }
 
 
-void Render::DrawCircle(vec2f postion, float radius, SDL_Color color)
+void Render::DrawCircle(const vec2f& postion, const float radius, const SDL_Color color)
 {
 	GPU_Circle(_instance->_screenTexture_target, postion.x, postion.y, radius, color);
 }
 
-void Render::DrawCircleFilled(vec2f postion, float radius, SDL_Color color)
+void Render::DrawCircleFilled(const vec2f& postion, const float radius, SDL_Color color)
 {
 	GPU_CircleFilled(_instance->_screenTexture_target, postion.x, postion.y, radius, color);
 }
 
-void Render::DrawTriangle(vec2f a, vec2f b, vec2f c, SDL_Color color)
+void Render::DrawTriangle(const vec2f& a, const vec2f& b, const vec2f& c, const SDL_Color color)
 {
 	GPU_Tri(_instance->_screenTexture_target, a.x, a.y, b.x, b.y, c.x, c.y, color);
 }
 
-void Render::DrawTriangleFilled(vec2f a, vec2f b, vec2f c, SDL_Color color)
+void Render::DrawTriangleFilled(const vec2f& a, const vec2f& b, const vec2f& c, const SDL_Color color)
 {
 	GPU_TriFilled(_instance->_screenTexture_target, a.x, a.y, b.x, b.y, c.x, c.y, color);
 }
 
 void Render::ApplyBloom()
 {
-	// wybiera tylko jasne kolory do textury
+	// select only bright pixels
 	GPU_ActivateShaderProgram(_shader_bright, &_shader_bright_block);
-	GPU_BlitRect(_screenTexture, NULL, _brightTexture_target, NULL);
+	GPU_BlitRect(_screenTexture, nullptr, _brightTexture_target, nullptr);
 
-	// aktywuje rozmycie
+	// active bloom
 	GPU_ActivateShaderProgram(_shader_bloom, &_shader_bloom_block);
 
-	// rozmazanie w pionie
+	// vertical bloom
 	GPU_SetUniformi(_shader_bloom_horizontal, 0);
-	GPU_BlitRect(_brightTexture, NULL, _bloomTexture_target, NULL);
-	// rozmazanie w poziomie
+	GPU_BlitRect(_brightTexture, nullptr, _bloomTexture_target, nullptr);
+	// horizontal bloom
 	GPU_SetUniformi(_shader_bloom_horizontal, 1);
-	GPU_BlitRect(_bloomTexture, NULL, _bloomTexture_target, NULL);
+	GPU_BlitRect(_bloomTexture, nullptr, _bloomTexture_target, nullptr);
 
-	GPU_ActivateShaderProgram(0, NULL);
-	//GPU_BlitRect(_bloomTexture, NULL, _screenTexture_target, NULL);
+	GPU_ActivateShaderProgram(0, nullptr);
 }
 
-void Render::DrawText(std::string text,FC_Font* font, vec2f pos, SDL_Color color)
+// Draw text, default align is left
+void Render::DrawText(const std::string& text,FC_Font* font, const vec2f& pos, const SDL_Color color)
 {
 	DrawTextAlign(text, font, pos, color, FC_ALIGN_LEFT);
 }
 
-void Render::DrawTextAlign(std::string text, FC_Font* font, vec2f pos, SDL_Color color, FC_AlignEnum align)
+// Draw text
+void Render::DrawTextAlign(const std::string& text, FC_Font* font, const vec2f& pos, const SDL_Color color, const FC_AlignEnum align)
 {
 	FC_DrawAlignColor(font, _instance->_screenTexture_target, pos.x, pos.y, align, color, text.c_str());
 }
 
+// Draw text
+void Render::DrawTextBox(const std::string& text, FC_Font* font, const GPU_Rect& box, const SDL_Color color, const FC_AlignEnum align)
+{
+	FC_DrawBoxEffect(font, _instance->_screenTexture_target, box, FC_MakeEffect(align, { 1.0f,1.0f }, color), text.c_str());
+}
+
+// Render cached texture to target
 void Render::RenderToTarget(GPU_Target* target)
 {
 	if (_instance->UseBloom) _instance->ApplyBloom();
-	//GPU_BlitRect(_instance->_screenTexture, NULL, target, NULL);
-	GPU_BlitRect(_instance->_screenTexture, NULL, target, NULL);
+	GPU_BlitRect(_instance->_screenTexture, nullptr, target, nullptr);
 
-	if (_instance->UseBloom)GPU_BlitRect(_instance->_bloomTexture, NULL, target, NULL);
+	if (_instance->UseBloom)GPU_BlitRect(_instance->_bloomTexture, nullptr, target, nullptr);
 }
 
+// clear all textures cache
 void Render::RenderClear()
 {
 	GPU_Clear(_instance->_screenTexture_target);
--- Render.h
+++ Render.h
@@ -20,24 +20,29 @@ public:
 	static void LoadShaders();
 
 	// drawing
-	static void DrawTexture(GPU_Image* texture, vec2f postion, vec2f scale, float angle, float alpha);
+	static void DrawTexture(GPU_Image* texture, const vec2f& postion, const vec2f& scale, float angle, float alpha);
 
-	static void DrawSprite(Sprite* sprite, vec2f postion, int frame);
-	static void DrawSprite_ex(Sprite* sprite, float posX, float posY, int frame, float scaleX, float scaleY, float centerX, float centerY, float angle, float alpha);
+	static void DrawSprite(Sprite* sprite, const vec2f& postion, int frame);
+	static void DrawSprite_ex(Sprite* sprite, float pos_x, float pos_y, int frame, float scale_x, float scale_y, float center_x, float center_y, float angle, float alpha);
+	static void DrawSpriteBox(const Sprite* sprite, GPU_Rect& box, int frame, const float& angle, float alpha);
 
-	static void DrawRect(Rect rect, SDL_Color color);
-	static void DrawRect_wh(Rect rect, SDL_Color color);
-	static void DrawRectFilled(Rect rect, SDL_Color color);
+	static void DrawRect(GPU_Rect rect, SDL_Color color);
+	static void DrawRect_wh(GPU_Rect rect, SDL_Color color);
+	static void DrawRectFilled(GPU_Rect rect, SDL_Color color);
 
-	static void DrawCircle(vec2f postion, float radius, SDL_Color color);
-	static void DrawCircleFilled(vec2f postion, float radius, SDL_Color color);
+	static void DrawRectRounded(GPU_Rect rect, float round_factor, SDL_Color color);
+	static void DrawRectRoundedFilled(GPU_Rect rect, float round_factor, SDL_Color color);
 
-	static void DrawTriangle(vec2f a, vec2f b, vec2f c, SDL_Color color);
-	static void DrawTriangleFilled(vec2f a, vec2f b, vec2f c, SDL_Color color);
+	static void DrawCircle(const vec2f& postion, float radius, SDL_Color color);
+	static void DrawCircleFilled(const vec2f& postion, float radius, SDL_Color color);
+
+	static void DrawTriangle(const vec2f& a, const vec2f& b, const vec2f& c, SDL_Color color);
+	static void DrawTriangleFilled(const vec2f& a, const vec2f& b, const vec2f& c, SDL_Color color);
 
 	// text
-	static void DrawText(std::string, FC_Font*, vec2f, SDL_Color);
-	static void DrawTextAlign(std::string, FC_Font*, vec2f, SDL_Color, FC_AlignEnum);
+	static void DrawText(const std::string&, FC_Font*, const vec2f&, SDL_Color);
+	static void DrawTextAlign(const std::string&, FC_Font*, const vec2f&, SDL_Color, FC_AlignEnum);
+	static void DrawTextBox(const std::string& text, FC_Font* font, const GPU_Rect& box, SDL_Color color, FC_AlignEnum align);
 
 	// system
 	static void RenderToTarget(GPU_Target* target);
@@ -60,10 +65,10 @@ private:
 	// shaders
 	Uint32 _shader_bloom;
 	Uint32 _shader_bloom_horizontal;
-	GPU_ShaderBlock _shader_bloom_block;
+	GPU_ShaderBlock _shader_bloom_block{};
 
 	Uint32 _shader_bright;
-	GPU_ShaderBlock _shader_bright_block;
+	GPU_ShaderBlock _shader_bright_block{};
 
 	// global screen
 	int width, height;
