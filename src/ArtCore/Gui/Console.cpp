90ffb3e55e964b0fcf051a8ddb506d5f6121f5af
atrox31 <atrox31@gmail.com>
2023-01-04 04:54:49 +0100

    add spy

--- src/ArtCore/Gui/Console.cpp
+++ src/ArtCore/Gui/Console.cpp
@@ -8,48 +8,66 @@
 #include "ArtCore/Scene/Scene.h"
 #include <ArtCore/predefined_headers/consola.h>
 
-Console::Console(): m_visibled(false), m_font(nullptr)
+#include "ArtCore/CodeExecutor/CodeExecutor.h"
+
+// must be static only for show list of functions
+std::map<std::string, void(*)(std::vector<std::string> args, int argc)> Console::_m_console_fun;
+
+Console::Console(): _m_visible(false), _m_font(nullptr)
 {
-	m_console_str = std::vector<std::string>();
-	m_console_str_history = std::vector<std::string>();
+	_m_console_str = std::vector<std::string>();
+	_m_console_str_history = std::vector<std::string>();
 }
 
 #define AddFunction(name,argc,body,info)														\
-m_console_fun[name]=[](std::vector<std::string> args, int arg){									\
-	if(argc+1 == arg){																			\
+_m_console_fun[name]=[](std::vector<std::string> args, int arg){									\
+	if((argc)+1 == arg){																			\
 		body;																					\
 		WrLn(info);																				\
 	}else{																						\
-		WrLn(std::string("Error, '")+name+"' require "+std::to_string(argc)+" arguments");		\
+		WrLn(std::string("Error, '")+(name)+"' require "+std::to_string(argc)+" arguments");		\
 	}																							\
 };																								\
 
 void Console::Init() {
-	m_font = FC_CreateFont();
-	FC_LoadFont_RW(m_font, SDL_RWFromConstMem(consola_ttf, 459181), 1, 16, C_BLACK, TTF_STYLE_NORMAL);
+	_m_font = FC_CreateFont();
+	FC_LoadFont_RW(_m_font, SDL_RWFromConstMem(consola_ttf, 459181), 1, 16, C_BLACK, TTF_STYLE_NORMAL);
 
-	AddFunction("pause", 0, Core::Pause() , "Game paused");
-	AddFunction("resume", 0, Core::Play() , "Game resume");
-	AddFunction("exit", 0, Core::GetInstance()->Exit();, "Game exit");
+	AddFunction("pause", 0, Core::Pause() , "Game paused")
+	AddFunction("resume", 0, Core::Play() , "Game resume")
+	AddFunction("exit", 0, Core::GetInstance()->Exit();, "Game exit")
 	AddFunction("spawn_instance", 3, {
 		Core::GetInstance()->_current_scene->CreateInstance(args[1], Func::TryGetFloat(args[2]), Func::TryGetFloat(args[3]));
-		}, "Game exit");
-
+		}, "Instance spawned")
+#ifdef _DEBUG
+	AddFunction("spy", 1, 
+		{
+			Core::GetInstance()->Executor->DebugSetInstanceToTrack(
+				Core::GetInstance()->_current_scene->GetInstanceById(Func::TryGetInt(args[1]))
+			);
+		}, "Get target info, press F4 to show info")
+#endif
+		AddFunction("help", 0, {
+					for (const std::string& function_name : _m_console_fun | std::views::keys)
+					{
+						WrLn(function_name);
+					}
+			}, "... end list")
 }
 
 Console::~Console()
 {
-	FC_FreeFont(m_font);
+	FC_FreeFont(_m_font);
 }
 
 void Console::ProcessTextInput(const std::string& txinput) {
-	m_console_buf += txinput;
-	m_console_shadow = "";
+	_m_console_buf += txinput;
+	_m_console_shadow = "";
 	std::string prediction;
-	for (const auto& key : m_console_fun | std::views::keys) {
+	for (const auto& key : _m_console_fun | std::views::keys) {
 		bool find = false;
-		for (int i = 0; i < std::min(m_console_buf.length(), key.length()); i++) {
-			if (key[i] == m_console_buf[i]) {
+		for (int i = 0; i < std::min(_m_console_buf.length(), key.length()); i++) {
+			if (key[i] == _m_console_buf[i]) {
 				find = true;
 			}
 			else {
@@ -61,102 +79,102 @@ void Console::ProcessTextInput(const std::string& txinput) {
 		}
 	}
 	if (prediction.length() > 0) {
-		m_console_shadow = prediction;
+		_m_console_shadow = prediction;
 	}
 	else {
-		m_console_shadow = "";
+		_m_console_shadow = "";
 	}
 }
 
 bool Console::ProcessKey(const SDL_KeyCode key)
 {
 	if (key == SDLK_HOME) {
-		m_visibled = !m_visibled;
-		if (m_visibled) Show(); else Hide();
+		_m_visible = !_m_visible;
+		if (_m_visible) Show(); else Hide();
 		return true;
 	}
-	if (!m_visibled) {
+	if (!_m_visible) {
 		return false;
 	}
 	switch (key) {
 	case SDLK_BACKSPACE:
 	{
-		if (m_console_buf.length() > 0) {
-			m_console_buf.pop_back();
-			if (m_console_buf.length() == 0) {
-				m_console_shadow = "";
+		if (_m_console_buf.length() > 0) {
+			_m_console_buf.pop_back();
+			if (_m_console_buf.length() == 0) {
+				_m_console_shadow = "";
 			}
 		}
 		else {
-			m_console_shadow = "";
+			_m_console_shadow = "";
 		}
 	}break;
 	case SDLK_RETURN:
 	{
-		if (m_console_buf.length() > 0) {
-			if (!m_console_str_history.empty() && m_console_str_history[0] != m_console_buf) {
-				m_console_str_history.insert(m_console_str_history.begin(), m_console_buf);
+		if (_m_console_buf.length() > 0) {
+			if (!_m_console_str_history.empty() && _m_console_str_history[0] != _m_console_buf) {
+				_m_console_str_history.insert(_m_console_str_history.begin(), _m_console_buf);
 			}
 			else {
-				if (m_console_str_history.empty()) {
-					m_console_str_history.insert(m_console_str_history.begin(), m_console_buf);
+				if (_m_console_str_history.empty()) {
+					_m_console_str_history.insert(_m_console_str_history.begin(), _m_console_buf);
 				}
 			}
-			Execute(m_console_buf);
-			m_console_buf = "";
-			m_console_shadow = "";
-			m_console_shadow_pos = 0;
-			m_console_page = 0;
+			Execute(_m_console_buf);
+			_m_console_buf = "";
+			_m_console_shadow = "";
+			_m_console_shadow_pos = 0;
+			_m_console_page = 0;
 		}
 	}break;
 	case SDLK_UP:
 	{
 		const Uint8* state = SDL_GetKeyboardState(nullptr);
 		if (state[SDL_SCANCODE_LCTRL]) {
-			if (m_console_page < int(m_console_str.size()) - m_console_block + 1) {
-				m_console_page++;
+			if (_m_console_page < int(_m_console_str.size()) - _m_console_block + 1) {
+				_m_console_page++;
 			}
 		}
 		else {
-			if (m_console_shadow_pos < int( m_console_str_history.size() )) {
-				m_console_buf = m_console_str_history[m_console_shadow_pos++];
+			if (_m_console_shadow_pos < int( _m_console_str_history.size() )) {
+				_m_console_buf = _m_console_str_history[_m_console_shadow_pos++];
 			}
 		}
 	}break;
 	case SDLK_DOWN: {
 		const Uint8* state = SDL_GetKeyboardState(nullptr);
 		if (state[SDL_SCANCODE_LCTRL]) {
-			if (m_console_page > 0) {
-				m_console_page--;
+			if (_m_console_page > 0) {
+				_m_console_page--;
 			}
 		}
 		else {
-			if (m_console_shadow_pos > 0) {
-				m_console_buf = m_console_str_history[--m_console_shadow_pos];
+			if (_m_console_shadow_pos > 0) {
+				_m_console_buf = _m_console_str_history[--_m_console_shadow_pos];
 
 			}
 		}
 	} break;
 	case SDLK_RIGHT: {
-		if (!m_console_shadow.empty()) {
-			m_console_buf = m_console_shadow;
+		if (!_m_console_shadow.empty()) {
+			_m_console_buf = _m_console_shadow;
 		}
 	}break;
 	}
-	return m_visibled;
+	return _m_visible;
 }
 
-void Console::WriteLine(std::string text)
+void Console::WriteLine(const std::string& text)
 {
-	m_console_str.insert(m_console_str.begin(), text + '\n');
+	_m_console_str.insert(_m_console_str.begin(), text + '\n');
 }
 
 void Console::Execute(const std::string& command)
 {
 	const std::vector<std::string> arg = Func::Explode(command, ' ');
-	if (m_console_fun.contains(arg[0])) {
-		WriteLine(std::move(command));
-		m_console_fun[arg[0]](arg, static_cast<int>(arg.size()));
+	if (_m_console_fun.contains(arg[0])) {
+		WriteLine(command);
+		_m_console_fun[arg[0]](arg, static_cast<int>(arg.size()));
 	}
 	else {
 		WriteLine("err: " + arg[0] + " - not found");
@@ -165,36 +183,36 @@ void Console::Execute(const std::string& command)
 
 void Console::RenderConsole()
 {
-	if (!m_visibled) return;
+	if (!_m_visible) return;
 	GPU_ActivateShaderProgram(0, nullptr);
-	const int block_size = FC_GetLineHeight(m_font) - 2;
+	const int block_size = FC_GetLineHeight(_m_font) - 2;
 	// draw back
 
 	const int _screen_height = Core::GetScreenHeight();
 	const int _screen_width = Core::GetScreenWidth();
 	GPU_RectangleFilled(Core::GetScreenTarget(), 0, static_cast<float>(_screen_height),
 	                    static_cast<float>(_screen_width),
-	                    static_cast<float>(_screen_height - block_size * m_console_block), C_DGRAY);
-	FC_SetDefaultColor(m_font, C_GREEN);
-	for (int i = 0; i < std::min(m_console_str.size(), (static_cast<size_t>(m_console_block) - 1)); i++)
+	                    static_cast<float>(_screen_height - block_size * _m_console_block), C_DGRAY);
+	FC_SetDefaultColor(_m_font, C_GREEN);
+	for (int i = 0; i < std::min(_m_console_str.size(), (static_cast<size_t>(_m_console_block) - 1)); i++)
 	{
-		FC_DrawAlign(m_font, Core::GetScreenTarget(), 2.0f,
+		FC_DrawAlign(_m_font, Core::GetScreenTarget(), 2.0f,
 		             static_cast<float>(_screen_height - (block_size) * (i + 2) - 4),
-		             FC_ALIGN_LEFT, m_console_str[static_cast<size_t>(i + m_console_page)].c_str());
+		             FC_ALIGN_LEFT, _m_console_str[static_cast<size_t>(i + _m_console_page)].c_str());
 	}
-	FC_SetDefaultColor(m_font, C_DGREEN);
-	FC_DrawAlign(m_font, Core::GetScreenTarget(), 2.0f,
+	FC_SetDefaultColor(_m_font, C_DGREEN);
+	FC_DrawAlign(_m_font, Core::GetScreenTarget(), 2.0f,
 	             static_cast<float>(_screen_height - (block_size) - 4),
-	             FC_ALIGN_LEFT, m_console_shadow.c_str());
-	FC_SetDefaultColor(m_font, C_GREEN);
-	m_cursor_interval += Core::GetInstance()->DeltaTime;
-	if (m_cursor_interval > 0.5)
+	             FC_ALIGN_LEFT, _m_console_shadow.c_str());
+	FC_SetDefaultColor(_m_font, C_GREEN);
+	_m_cursor_interval += Core::GetInstance()->DeltaTime;
+	if (_m_cursor_interval > 0.5)
 	{
-		m_cursor_interval = 0.0;
-		m_show_cursor = !m_show_cursor;
+		_m_cursor_interval = 0.0;
+		_m_show_cursor = !_m_show_cursor;
 	}
-	std::string content = ">" + m_console_buf + (m_show_cursor ? "|" : "");
-	FC_DrawAlign(m_font, Core::GetInstance()->GetScreenTarget(), 2.0f,
+	std::string content = ">" + _m_console_buf + (_m_show_cursor ? "|" : "");
+	FC_DrawAlign(_m_font, Core::GetInstance()->GetScreenTarget(), 2.0f,
 	             static_cast<float>(_screen_height - (block_size) - 4),
 	             FC_ALIGN_LEFT, content.c_str());
 }
@@ -202,21 +220,21 @@ void Console::RenderConsole()
 void Console::Show()
 {
 	SDL_StartTextInput();
-	m_visibled = true;
-	m_console_buf = "";
-	m_console_shadow = "";
-	m_console_shadow_pos = 0;
-	m_console_page = 0;
-	m_console_block = 12;
+	_m_visible = true;
+	_m_console_buf = "";
+	_m_console_shadow = "";
+	_m_console_shadow_pos = 0;
+	_m_console_page = 0;
+	_m_console_block = 12;
 }
 
 void Console::Hide()
 {
 	SDL_StopTextInput();
-	m_visibled = false;
+	_m_visible = false;
 }
 
-void Console::WrLn(std::string msg)
+void Console::WrLn(const std::string& line)
 {
-	Core::GetInstance()->Consola->WriteLine(msg);
+	Core::GetInstance()->Consola->WriteLine(line);
 }
--- src/ArtCore/Gui/Console.h
+++ src/ArtCore/Gui/Console.h
@@ -18,31 +18,32 @@ public:
 	// return true if hadled
 	bool ProcessKey(SDL_KeyCode key);
 
-	void WriteLine(std::string text);
+	void WriteLine(const std::string& text);
 	void Execute(const std::string& command);
 
 	void RenderConsole();
 	void Show();
 	void Hide();
 
-	bool IsShown() {
-		return m_visibled;
-	};
+	[[nodiscard]] bool IsShown() const
+	{
+		return _m_visible;
+	}
 
 private:
-	void static WrLn(std::string);
+	void static WrLn(const std::string& line);
 	// console functions
-	bool m_show_cursor = false;
-	double m_cursor_interval = 0.0;
-	bool m_visibled;
-	std::vector<std::string> m_console_str;
-	std::vector<std::string> m_console_str_history;
-	std::string m_console_buf = "";
-	std::string m_console_shadow = "";
-	int m_console_shadow_pos = 0;
-	int m_console_page = 0;
-	int m_console_block = 12;
-	std::map<std::string, void(*)(std::vector<std::string> args, int argc)> m_console_fun;
-	FC_Font* m_font;
+	bool _m_show_cursor = false;
+	double _m_cursor_interval = 0.0;
+	bool _m_visible;
+	std::vector<std::string> _m_console_str;
+	std::vector<std::string> _m_console_str_history;
+	std::string _m_console_buf;
+	std::string _m_console_shadow;
+	int _m_console_shadow_pos = 0;
+	int _m_console_page = 0;
+	int _m_console_block = 12;
+	static std::map<std::string, void(*)(std::vector<std::string> args, int argc)> _m_console_fun;
+	FC_Font* _m_font;
 };
 
