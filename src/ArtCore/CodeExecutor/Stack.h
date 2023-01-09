610f6982bde0031a9db43dd8130a9a85a4384e8e
atrox31 <atrox31@gmail.com>
2023-01-10 00:11:34 +0100

    upgrade stack to 3 times faster struct

--- src/ArtCore/CodeExecutor/Stack.h
+++ src/ArtCore/CodeExecutor/Stack.h
@@ -1,8 +1,64 @@
 #pragma once
-#include <vector>
-
+#include "../src/plf/plf_stack-master/plf_stack.h"
 template <typename T>
 struct AStack final
+{
+private:
+	plf::stack<T> _data;
+public:
+	// Add variable to stack
+	void Add(T value) {
+		_data.emplace(value);
+	}
+	// Get fresh value on stack
+	T Get() {
+		if (_data.size() > 0) {
+			T tmp(std::move(_data.top()));
+			_data.pop();
+			return tmp;
+		}else
+		{
+			return {};
+		}
+	}
+
+	[[nodiscard]] bool IsEmpty() const
+	{
+		return _data.empty();
+	}
+
+	[[nodiscard]] int Size() const
+	{
+		return static_cast<int>(_data.size());
+	}
+
+	[[nodiscard]] int Capacity() const
+	{
+		return static_cast<int>(_data.capacity());
+	}
+	void Erase() {
+		_data.clear();
+	}
+
+	// Delete last element on stack
+	void PopBack()
+	{
+		_data.pop();
+	}
+
+	AStack(size_t default_stack_size = 8) {
+		_data.reserve(default_stack_size);
+	}
+	~AStack() {
+		_data.clear();
+	}
+};
+
+// old implementation, from test then 60ms to 1.3 ms in 10 000 iteration
+
+#include <vector>
+template <typename T>
+struct old_AStack final
 {
 private:
 	size_t _size;
@@ -61,13 +117,13 @@ public:
 		}
 	}
 
-	AStack(size_t default_stack_size = 8) {
+	old_AStack(size_t default_stack_size = 8) {
 		_size = 0;
 		_c_element = 0;
 		_data.reserve(default_stack_size);
 		_current_capacity = default_stack_size;
 	}
-	virtual ~AStack() {
+	virtual ~old_AStack() {
 		_size = 0;
 		_c_element = 0;
 		_data.clear();
