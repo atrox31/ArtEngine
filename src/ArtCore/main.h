242449e27ee7d3a44dedf90d812e6c3eebf91fdf
atrox31 <atrox31@gmail.com>
2023-01-24 19:27:52 +0100

    add debug flag

--- src/ArtCore/main.h
+++ src/ArtCore/main.h
@@ -6,3 +6,8 @@ static constexpr int VERSION_PATH = 0;
 // required Compiler code version
 static constexpr int MINIMUM_ART_COMPILER_MAIN = 1;
 static constexpr int MINIMUM_ART_COMPILER_MINOR = 8;
+
+// all debug definitions
+
+// allow debugging in release mode to enable view debug data
+	#define AC_ENABLE_DEBUG_MODE 1
